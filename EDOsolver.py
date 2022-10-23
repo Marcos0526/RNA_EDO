c69598903809001eb7b411b0c578885cfb626380
Marcos Bautista M <marcosbautista@iMac-de-Marcos.local>
2022-10-23 01:09:55 -0500

    Se corrigio el codigo y ahora ya se ejecuta

--- EDOsolver.py
+++ EDOsolver.py
@@ -16,33 +16,32 @@ class ODEsolver(Sequential):
         self.loss_tracker = keras.metrics.Mean(name = "loss")
 
 
+    @property
+    def metrics(self):
+      return [self.loss_tracker]
 
-def metrics(self):
-    return [self.loss_tracker]
+    def train_step(self, data):
+        batch_size = tf.shape(data)[0]
+        x= tf.random.uniform((batch_size,1), minval =-2, maxval =2)
 
-def train_step(self, data):
-    batch_size = tf.shape(data)[0]
-    x= tf.random.uniform((batch_size,1), minval =-2, maxval =2)
+        with tf.GradientTape() as tape:
 
-    with tf.GradientTape() as tape:
+            with tf.GradientTape() as tape2:
+                tape2.watch(x)
+                y_pred = self(x,training = True)
+            dy= tape2.gradient(y_pred,x)
+            x_o = tf.zeros((batch_size,1))
+            y_o = self(x_o, training=True)
+            eq = dy+2.*x*y_pred
+            ic = y_o - 1.
+            loss = keras.losses.mean_squared_error(0., eq) +keras.losses.mean_squared_error(0., ic)
 
-        with tf.GradientTape() as tape2:
-            tape2.watch(x)
-            y_pred = self(x,training = True)
-        dy= tape2.gradient(y_pred,x)
-        x_o = tf.zeros((batch_size,1))
-        y_o = self(x_o, training=True)
-        eq = dy+2.*x*y_pred
-        ic = y_o - 1.
-        loss = keras.losses.mean_squared_error(0., eq) +keras.losses.mean_squared_error(0., ic)
+        grads = tape.gradient(loss , self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
 
-    grads = tape.gradient(loss , self.trainable_variables)
-    self.optimizer.apply_gradient(zip(grads, slef.trainable_variables))
+        self.loss_tracker.update_state(loss)
 
-
-    self.loss_tracker.update_state(loss)
-
-    return {"loss": self.loss_tracker.result()}
+        return {"loss": self.loss_tracker.result()}
 
 model = ODEsolver()
 
@@ -54,18 +53,18 @@ model.add(Dense(1, activation = 'linear'))
 model.summary()
 
 
-model.compile(optimizer = RMSprop(),metrics=['loss'])
+model.compile(optimizer=RMSprop(),metrics=['loss'])
 
 
 x=tf.linspace(-2,2,100)
 history = model.fit(x,epochs=500,verbose=1)
 
 
-x_testv = tf.linespace(-2,2,100)
-a = model1.predict(x_testv)
+x_testv = tf.linspace(-2,2,100)
+a = model.predict(x_testv)
 plt.plot(x_testv,a)
 plt.plot(x_testv,np.exp(-x*x))
-plot.show()
+plt.show()
 exit()
 
 
